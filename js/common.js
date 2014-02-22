b57dffb752d8ee0add8c4aea349b6c13730782b2
narsi <narsihuang@gmail.com>
2014-02-22 17:50:26 +0800

    恢复评论框为 Disqus

--- js/common.js
+++ js/common.js
@@ -88,15 +88,14 @@ $(function(){
 })
 
 // Disqus 加载
-/*
 $(function(){
-    $("#show_disqus").click(function(){
+//    $("#show_disqus").click(function(){
         window.disqus_shortname = 'narsi'; 
         $(this).html('加载中...');
         var that = this;
         $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
-    });
-    
+ //   });
+
     //添加 Google-code-preffity 需要的 class
     $('pre').addClass('prettyprint linenums');
     
@@ -104,9 +103,10 @@ $(function(){
         prettyPrint();
     });
 })
-*/
+
 
 // 多说
+/*
 var duoshuoQuery = {short_name:"narsi"};
 (function(){
     var ds = document.createElement('script');
@@ -116,3 +116,4 @@ var duoshuoQuery = {short_name:"narsi"};
     (document.getElementsByTagName('head')[0] 
     || document.getElementsByTagName('body')[0]).appendChild(ds);
 })();
+*/
